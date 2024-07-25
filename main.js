f2c754250c2a18dcdffa03e8344f5d8ae004272e
joaopedro098 <163419020+joaopedro098@users.noreply.github.com>
2024-07-25 12:48:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let jogador=[5]
+let idade=[5]
+let altura=[5]
+for(let i=0;i<=1;i++){
+      jogador[i]= prompt(`informe o nome do ${i+1}º jogador`)
+      idade[i] = prompt(`informe a idade do ${i+1}º jogador`)
+      altura[i] = prompt(`informe a altura do ${i+1}º jogador`)}
+      
+
+
+for(let i=0;i<=1;i++){
+    alert(`o nome do jogador é:${jogador[i]}, a idade do jogador é:${idade[i]}, a altura do jogador é${altura[i]}`)
+}
